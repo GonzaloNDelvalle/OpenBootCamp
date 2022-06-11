3d9ea21328f40098eac31c5b480127eb83e32f8f
Gonzalo <dgonzalo@fi.uba.ar>
2022-06-11 20:44:09 -0300

    Ejercicio3

---
+++ funciones_3.py
@@ -0,0 +1,11 @@
+def bisiesto(anio):
+    if anio % 4 == 0:
+        if anio % 100 == 0:
+            if anio % 400 == 0:
+                print("El año es bisiesto")
+            else:
+                print("El año no es bisiesto")
+        else:
+            print("El año es bisiesto")
+    else:
+        print("El año no es bisiesto")
