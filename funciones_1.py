b2f92ae3ebe5fb525282724bd51bbe2e8babe2c0
Gonzalo <dgonzalo@fi.uba.ar>
2022-06-11 19:53:00 -0300

    Ejercicio1

---
+++ funciones_1.py
@@ -0,0 +1,9 @@
+def area_triangulo(base, altura):
+    return (base * altura)/2
+
+def area_circulo(radio):
+    pi = 3.14
+    return (pi * (radio**2))
+
+print(area_triangulo(3, 4))
+print(area_circulo(3))
