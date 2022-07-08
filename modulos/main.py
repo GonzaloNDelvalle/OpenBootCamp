1c616b731067781b21a02e3ac4540d399367ac9b
Gonzalo <dgonzalo@fi.uba.ar>
2022-07-08 02:44:16 -0300

    Paquetes y Modulos

---
+++ modulos/main.py
@@ -0,0 +1,11 @@
+from paquete import operaciones, clases
+import math
+
+def main():
+    resultado = operaciones.suma(3, 5)
+    print(resultado)
+
+    gonzalo = clases.Alumno()
+    gonzalo.imprimir()
+
+main()
